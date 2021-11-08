38cbd463c71f0d415da0c660c81e9ef0914d6202
Pablo Bellido <pablobellido@uma.es>
2021-11-08 21:14:45 +0100

    Ejercicios resueltos de Divide y Vencerás

---
+++ Ejercicios/Tema 3/examen/Ejercicio1.java
@@ -0,0 +1,53 @@
+package examen;
+
+// Ejercicio correspondiente al primer parcial del curso 19-20
+// Mismo ejercicio preguntado en el curso 18-19 aunque con la diferencia de A[0] si puede ser 0
+
+public class Ejercicio1 {
+	
+	public static void main(String [] args) {
+		
+		// Ejemplos 19-20
+		int [] v1 = {21,22,24,30,36,37};
+		int [] v2 = {21,22,23,24,25,26};
+		
+		int sol1 = menor(v1,0,v1.length-1);
+		int sol2 = menor(v2,0,v2.length-1);
+		
+		System.out.println(sol1 + " - " + sol2 + "\r\n");
+		
+		// Ejemplos 18-19
+		int [] a1 = {0,1,2,6,9,11,15};
+		int [] a2 = {1,2,3,4,6,9,11,15};
+		int [] a3 = {0,1,2,3,4,5,6};
+		
+		int res1 = menor(a1, 0, a1.length-1);
+		int res2 = menor(a2,0,a2.length-1);
+		int res3 = menor(a3, 0, a3.length-1);
+		
+		System.out.println(res1 + " - " + res2 + " - " + res3);
+ 		
+	}
+	
+	public static int menor(int[] A, int inf, int sup) {
+		
+		int menor = 0;
+		
+		if(A[sup] - A[inf] == sup - inf) return A[sup]+1;
+
+		while(inf<sup) {
+			
+			int m = (inf+sup)/2;
+
+			if((A[m] - m) == A[0]) return menor(A, m+1, sup);
+			
+			else return menor(A, inf, m-1);
+			
+		}
+		
+		return menor;
+	}
+
+
+}
+
---
+++ Ejercicios/Tema 3/examen/Ejercicio2.java
@@ -0,0 +1,66 @@
+package examen;
+
+import java.util.Arrays;
+
+// Ejercicio correspondiente al primer parcial del curso 20-21.
+// Este ejercicio nos pide determinar si el vector s es subvector de v
+
+public class Ejercicio2 {
+
+    public static void main(String [] args){
+
+        int [] v = {-10,-3,0,4,7,19,33};
+        int [] s = {4,7,19};
+
+        if(fuerzaBruta(v, s)) System.out.println("El algoritmo por Fuerza Bruta determina que s es un subarray de v \r\n");
+        
+        if(subArray(v, s, 0, v.length-1)) System.out.println("La primera implementación del algoritmo por DyV determina que s es un subarray de v \r\n");
+
+        if(subArrayBinary(v, s)) System.out.println("La segunda implementación del algoritmo por DyV determina que s es un subarray de v \r\n");
+
+    }
+
+    // Implementación por Fuerza Bruta --> T(n) € O(n)
+    public static boolean fuerzaBruta(int[]v, int[]s){
+        int i=0;
+        int j=0;
+
+        while(i<v.length && v[i] != s[0]) i++;
+        while(j<s.length && v[i+j]==s[j]) j++;
+
+        return j == s.length;
+    } 
+
+    // Implementación 1 por Divide y Vencerás --> T(n) € O(log n)
+    // En esta implementación nos fabricamos nuestra propia versión de la busqueda binaria.
+    public static boolean subArray(int[]v, int[]s, int inf, int sup){
+        if(inf >= sup) return false;
+
+        if(inf<sup){
+            int pm = (inf+sup) / 2;
+
+            if(v[pm] == s[0]) return compararArrays(v,s,pm);
+            if(v[pm] > s[0]) return subArray(v, s, inf, pm-1);
+            else return subArray(v, s, pm+1, sup);
+
+        } return false;
+    }
+
+    // Implementación 2 por Divide y Vencerás T(n) € O(log n)
+    // En esta implementación utilizamos la clase BinarySearch predefinida en Java
+    public static boolean subArrayBinary(int[]v, int[]s){
+        int pos = Arrays.binarySearch(v, 0, v.length-1, s[0]);
+
+        if(pos==-1) return false;
+        else return compararArrays(v, s, pos);
+    }
+
+
+    // El enunciado nos dice que el vector s es de longitud constante k por tanto este método es constante T(n) --> O(1)
+    private static boolean compararArrays(int[]v, int[]s, int pm){
+        int i=0;
+        while(i<s.length && v[pm+i] == s[i]) i++;
+        return i == s.length;
+    }
+    
+}
